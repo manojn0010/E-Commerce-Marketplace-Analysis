e730ea5ed5c18d2ed48c1ec1537778e350157c84
manojn0010 <manojkn2123@gmail.com>
2026-02-06 00:30:48 +0530

    Add run_all.sql to execute multiple SQL scripts

---
+++ scripts/run_all/00_run_all.sql
@@ -0,0 +1,7 @@
+SOURCE scripts/01_schema_and_tables.sql;
+SOURCE scripts/02_load_data.sql;
+SOURCE scripts/03_null_handling.sql;
+SOURCE scripts/04_data_normalisation.sql;
+SOURCE scripts/05_procedures_and_functions.sql;
+SOURCE scripts/06_views.sql;
+SOURCE scripts/07_kpis_answered.sql;
