a91c0ad5209b5d955e82f62b90887e6835080533
Manoj N <manojkn2123@gmail.com>
2026-02-18 10:34:15 +0530

    Update v0_connect_mysql.R

--- scripts/visualisation/v0_connect_mysql.R
+++ scripts/visualisation/v0_connect_mysql.R
@@ -1,8 +1,10 @@
-# install the necessary packages
-install.packages(c("DBI", "RMySQL"))  # for database handling
-install.packages(c("dplyr", "ggplot2", "zoo"))  # for cleaning, plotting and time-series plots
-install.packages("shiny") # for interactive plots
+# File: v0_connect_mysql.R
+# Project: E-COM Analytics  
 
+# Install the Necessary Packages 
+install.packages(c("DBI", "RMySQL"))            # for database handling  
+install.packages(c("dplyr", "ggplot2", "zoo"))  # for cleaning, plotting and time-series plots  
+install.packages("shiny")                       # for interactive plots  
 
 library(DBI)
 library(RMySQL)
