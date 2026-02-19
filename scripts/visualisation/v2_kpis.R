228b5163756fa6a8a2a651b1a0e5a7e7765a8b53
Manoj N <manojkn2123@gmail.com>
2026-02-20 01:43:32 +0530

    Update and rename v1_kpis.R to v2_kpis.R

--- scripts/visualisation/v2_kpis.R
+++ scripts/visualisation/v2_kpis.R
@@ -1,5 +1,11 @@
+# File: v2_kpis.R
+# Project: E-COM Analytics  
+# Purpose: Load KPIs as Objects 
+
+# Load necessary libraries
 library(dplyr)
 
+# 1. Gross Merchandise Value
 gmv <- dbGetQuery(con, "
                   select 
                   min(day_of_purchase) as from_date,
@@ -8,7 +14,8 @@ gmv <- dbGetQuery(con, "
                   from base_orders
                   where cost is not null
                   ")
- 
+
+# 2. Order Count and Average Order value
 gmv_vs_aov <- dbGetQuery(con, "
                          select
                          count(distinct o_id) as order_count,
@@ -17,6 +24,7 @@ gmv_vs_aov <- dbGetQuery(con, "
                          from order_costs
                          where order_cost is not null")
 
+# 3. High-Value Seller Metrics
 high_value_sellers <- dbGetQuery(con, "
                                  with temp as(
                                  select
@@ -37,6 +45,7 @@ high_value_summary <- high_value_sellers %>%
     high_value_sellers_avg_gmv = round(mean(individual_value), 2)
   )
 
+# 4. Top 3 States for each Category
 top_states_by_category <- dbGetQuery(con, "
                                      select *
                                      from state_rank_by_category
@@ -44,7 +53,9 @@ top_states_by_category <- dbGetQuery(con, "
                                      and p_category != 'unknown'
                                      ")
 top_states_by_category <- top_states_by_category %>% mutate(p_category=trimws(p_category))
+# Later made into an interactive plot using shiny.
 
+# 5. State-wise GMV Share
 state_gmv_share <- dbGetQuery(con, "
                               select 
                               state, 
@@ -52,8 +63,10 @@ state_gmv_share <- dbGetQuery(con, "
                               from state_ttl_price_rankings
                               ")
 
+# 6. Each State Month-on-Month Growth
 state_growth_by_mnt <- dbGetQuery(con, "
                                   select *,
                                   rev-lag(rev) over(partition by state order by mnt) as growth_from_prev_mnt
                                   from rolling_rev_by_state
                                   ")
+# Later made into an interactive plot using shiny.
