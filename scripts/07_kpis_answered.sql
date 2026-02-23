8377118537c1f26a38f930ad9ae1a2a0307846d2
Manoj N <manojkn2123@gmail.com>
2026-02-23 07:34:08 +0530

    Update 07_kpis_answered.sql

--- scripts/07_kpis_answered.sql
+++ scripts/07_kpis_answered.sql
@@ -5,10 +5,10 @@
 -- Answer KPIs
  -- 1 What is the overall Gross Merchandise Value?
  -- 2 How GMV compares to Average Order Value using Order Count?
- -- 3 Metrics of Sellers with more than 10 orders
+ -- 3 Top 10 Statewise Revenue Share
  -- 4 Which are the top 3 states for each Product Category?
- -- 5 How much each State contributes towards total GMV?
- -- 6 Monthly Revenue Growth for each State
+ -- 5 Category Ranks by Revenue
+ -- 6 Monthly Revenue Trend
 -- =========================
 -- 1 gmv
 select 
@@ -26,30 +26,30 @@ round(sum(order_cost)/count(*), 2) as aov
 from order_costs
 where order_cost is not null;
 -- -------------------------
--- 3 high_value_seller_metrics
-with temp as (
-    select s_id, sum(cost) as individual_value
-    from base_orders
-    where cost is not null
-    group by s_id
-    having count(s_id) > 10
-)
-select 
-count(*) as high_value_seller_cnt, 
-sum(individual_value) as seller_gmv, 
-round(sum(individual_value)/count(*), 2) as high_value_sellers_avg_gmv
-from temp;
+-- 3 top 10 states by revenue
+select *, round(100*state_rev/(sum(state_rev) over ()), 2)as gmv_percent_share 
+from
+(select state, sum(rev) as state_rev 
+from rolling_rev_by_state
+group by state) b
+group by state
+order by state_rev desc
+limit 10;
 -- -------------------------
 -- 4 top 3 states for each category by rating
 select * 
 from state_rank_by_category
 where rnk <= 3;
 -- -------------------------
--- 5 state_gmv_percent_share
-select state, round(100*ttl_price/sum(ttl_price) over (), 2) as seller_state_gmv_share
-from state_ttl_price_rankings;
+-- 5 category_rev_ranks
+select *, rank () over (order by rev desc)
+from category_rank_by_rev
+where category != 'UNKNOWN';
 -- -------------------------
--- 6 growth_over_month
-select *, (rev) - lag(rev) over(partition by state order by mnt) as growth_from_prev_mnt 
-from rolling_rev_by_state;
+-- 6 Platform Revenue Trend
+select date_format(day_of_purchase, '%y-%m') as mnt, 
+sum(cost) as rev
+from base_orders
+group by mnt
+order by mnt;
 -- =========================
